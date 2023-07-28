163f20a77ec5770c013ba078c44041b70dea70ca
Kamalesh <kamaleshkakarla@gmail.com>
2023-07-28 11:19:02 +0530

    Adding functions

--- common.sh
+++ common.sh
@@ -76,20 +76,20 @@ func_java(){
 func_python(){
 
   echo -e "\e[36m <<<<<<<<<< creating a paytm service file >>>>>>>>>>\e[0m" | tee -a ${log}
-  cp ${component}.service /etc/systemd/system/${component}.service
+  cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
   echo -e "\e[36m <<<<<<<<<< python installation >>>>>>>>>>\e[0m" | tee -a ${log}
-  yum install python36 gcc python3-devel -y
+  yum install python36 gcc python3-devel -y &>>${log}
   echo -e "\e[36m <<<<<<<<<< removing existing app >>>>>>>>>>\e[0m" | tee -a ${log}
-  rm -rf cd /app
+  rm -rf cd /app &>>${log}
   echo -e "\e[36m <<<<<<<<<< adding user and creating app >>>>>>>>>>\e[0m" | tee -a ${log}
-  useradd roboshop
-  mkdir /app
+  useradd roboshop &>>${log}
+  mkdir /app &>>${log}
   echo -e "\e[36m <<<<<<<<<< Installing app code >>>>>>>>>>\e[0m" | tee -a ${log}
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-  cd /app
-  unzip /tmp/${component}.zip
-  cd /app
-  pip3.6 install -r requirements.txt
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+  cd /app &>>${log}
+  unzip /tmp/${component}.zip &>>${log}
+  cd /app &>>${log}
+  pip3.6 install -r requirements.txt &>>${log}
   echo -e "\e[36m <<<<<<<<<< restarting the services >>>>>>>>>>\e[0m" | tee -a ${log}
   func_systemd
   

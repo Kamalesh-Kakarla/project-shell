60edf15c4cb4e42e62349461014714eae0d9c2c1
Kamalesh-Kakarla <48642647+Kamalesh-Kakarla@users.noreply.github.com>
2023-08-05 00:34:56 +0530

    Update common.sh

--- common.sh
+++ common.sh
@@ -1,100 +1,129 @@
 log=/tmp/roboshop.log
 
-func_apppreq(){
-
-  echo -e "\e[36m <<<<<<<<<< creating app directory >>>>>>>>>>\e[0m" | tee -a ${log}
-  mkdir /app &>>${log}
-  echo -e "\e[36m <<<<<<<<<< creating a zip file >>>>>>>>>\e[0m" | tee -a ${log}
-  curl - L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-  echo -e "\e[36m <<<<<<<<<< changing to app directory >>>>>>>>>>\e[0m" | tee -a ${log}
-  cd /app &>>${log}
-  echo -e "\e[36m <<<<<<<<<< unzip the ${component} file >>>>>>>>>>\e[0m" | tee -a ${log}
-  unzip /tmp/${component}.zip &>>${log}
-  echo -e "\e[36m <<<<<<<<<< changing to app directory >>>>>>>>>>\e[0m" | tee -a ${log}
-  cd /app &>>${log}
-
+func_exit_status() {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[31m FAILURE \e[0m"
+  fi
 }
 
-func_systemd(){
-  systemctl daemon-reload
-  systemctl enable ${component}
-  systemctl restart ${component}
-}
-
-func_nodejs(){
-
-log=/tmp/${component}.log
-d='date'
-echo "The script starts at -" ; $d #| tee -a ${log}
-echo -e "\e[36m <<<<<<<<<< creating a ${component} service file >>>>>>>>>>\e[0m" | tee -a ${log}
-cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
-echo -e "\e[36m <<<<<<<<<< creating mongodb repo >>>>>>>>>>\e[0m" | tee -a ${log}
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
-echo -e "\e[36m <<<<<<<<<< Installing rpms from internet >>>>>>>>>>\e[0m" | tee -a ${log}
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
-echo -e "\e[36m <<<<<<<<<< installing nodejs >>>>>>>>>>\e[0m" | tee -a ${log}
-yum install nodejs -y &>>${log}
-echo -e "\e[36m <<<<<<<<<< adding robo shop user >>>>>>>>>>\e[0m" | tee -a ${log}
-useradd roboshop &>>${log}
-echo -e "\e[36m <<<<<<<<<< removing app directory >>>>>>>>>>\e[0m" | tee -a ${log}
-rm -rf /app &>>${log}
-
-func_apppreq
-
-echo -e "\e[36m <<<<<<<<<< npms installing >>>>>>>>>>\e[0m" | tee -a ${log}
-npm install &>>${log}
-echo -e "\e[36m <<<<<<<<<< installing the mongodb org >>>>>>>>>>>\e[0m" | tee -a ${log}
-yum install mongodb-org-shell -y &>>${log}
-mongo --host mongodb.kkakarla.online </app/schema/${component}.js &>>${log}
-
-echo -e "\e[36m <<<<<<<<<< restarting the services >>>>>>>>>>\e[0m" | tee -a ${log}
-
-func_systemd
-}
-
-func_java(){
-
-  echo -e "\e[36m <<<<<<<<<< creating service file >>>>>>>>>>\e[0m" | tee -a ${log}
+func_apppreq() {
+  echo -e "\e[36m>>>>>>>>>>>>  Create ${component} Service  <<<<<<<<<<<<\e[0m"
   cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
-  echo -e "\e[36m <<<<<<<<<< Installing Maven and creating user roboshop >>>>>>>>>>\e[0m" | tee -a ${log}
-  yum install maven -y &>>${log}
-  useradd roboshop &>>${log}
+  func_exit_status
 
-  rm -rf cd /app
+  echo -e "\e[36m>>>>>>>>>>>>  Create Application User  <<<<<<<<<<<<\e[0m"
+  id roboshop &>>${log}
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>${log}
+  fi
+  func_exit_status
+
+  echo -e "\e[36m>>>>>>>>>>>>  Cleanup Existing Application Content  <<<<<<<<<<<<\e[0m"
+  rm -rf /app &>>${log}
+  func_exit_status
+
+  echo -e "\e[36m>>>>>>>>>>>>  Create Application Directory  <<<<<<<<<<<<\e[0m"
+  mkdir /app &>>${log}
+  func_exit_status
+
+  echo -e "\e[36m>>>>>>>>>>>>  Download Application Content  <<<<<<<<<<<<\e[0m"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+  func_exit_status
+
+  echo -e "\e[36m>>>>>>>>>>>>  Extract Application Content  <<<<<<<<<<<<\e[0m"
+  cd /app
+  unzip /tmp/${component}.zip &>>${log}
+  func_exit_status
+}
+
+func_systemd() {
+  echo -e "\e[36m>>>>>>>>>>>>  Start ${component} Service  <<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+  systemctl daemon-reload &>>${log}
+  systemctl enable ${component} &>>${log}
+  systemctl restart ${component} &>>${log}
+  func_exit_status
+}
+
+func_schema_setup() {
+  if [ "${schema_type}" == "mongodb" ]; then
+    echo -e "\e[36m>>>>>>>>>>>>  INstall Mongo Client  <<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+    yum install mongodb-org-shell -y &>>${log}
+    func_exit_status
+
+    echo -e "\e[36m>>>>>>>>>>>>  Load User Schema  <<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+    mongo --host mongodb.rdevopsb72.online </app/schema/${component}.js &>>${log}
+    func_exit_status
+  fi
+
+  if [ "${schema_type}" == "mysql" ]; then
+    echo -e "\e[36m>>>>>>>>>>>>  Install MySQL Client   <<<<<<<<<<<<\e[0m"
+    yum install mysql -y &>>${log}
+    func_exit_status
+
+    echo -e "\e[36m>>>>>>>>>>>>  Load Schema   <<<<<<<<<<<<\e[0m"
+    mysql -h mysql.rdevopsb72.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
+    func_exit_status
+  fi
+
+}
+
+func_nodejs() {
+  log=/tmp/roboshop.log
+
+  echo -e "\e[36m>>>>>>>>>>>>  Create MongoDB Repo  <<<<<<<<<<<<\e[0m"
+  cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+  func_exit_status
+
+  echo -e "\e[36m>>>>>>>>>>>>  Install NodeJS Repos  <<<<<<<<<<<<\e[0m"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+ func_exit_status
+
+  echo -e "\e[36m>>>>>>>>>>>>  Install NodeJS  <<<<<<<<<<<<\e[0m"
+  yum install nodejs -y &>>${log}
+  func_exit_status
 
   func_apppreq
 
-  echo -e "\e[36m <<<<<<<<<< cleaning packages >>>>>>>>>>\e[0m" | tee -a ${log}
-  mvn clean package &>>${log}
-  echo -e "\e[36m <<<<<<<<<< moving jar files >>>>>>>>>>\e[0m" | tee -a ${log}
-  mv target/${component}-1.0.jar ${component}.jar &>>${log}
-  echo -e "\e[36m <<<<<<<<<< Installing mysql >>>>>>>>>>\e[0m" | tee -a ${log}
-  yum install mysql -y &>>${log}
-  mysql -h mysql.kkakarla.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Download NodeJS Dependencies  <<<<<<<<<<<<\e[0m"
+  npm install &>>${log}
+  func_exit_status
 
-  echo -e "\e[36m <<<<<<<<<< restarting the services >>>>>>>>>>\e[0m" | tee -a ${log}
+  func_schema_setup
 
   func_systemd
 }
 
-func_python(){
+func_java() {
+  echo -e "\e[36m>>>>>>>>>>>>  Install Maven   <<<<<<<<<<<<\e[0m"
+  yum install maven -y &>>${log}
+  func_exit_status
 
-  echo -e "\e[36m <<<<<<<<<< creating a paytm service file >>>>>>>>>>\e[0m" | tee -a ${log}
-  cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
-  echo -e "\e[36m <<<<<<<<<< python installation >>>>>>>>>>\e[0m" | tee -a ${log}
-  yum install python36 gcc python3-devel -y &>>${log}
-  echo -e "\e[36m <<<<<<<<<< removing existing app >>>>>>>>>>\e[0m" | tee -a ${log}
-  rm -rf cd /app &>>${log}
-  echo -e "\e[36m <<<<<<<<<< adding user and creating app >>>>>>>>>>\e[0m" | tee -a ${log}
-  useradd roboshop &>>${log}
-  mkdir /app &>>${log}
-  echo -e "\e[36m <<<<<<<<<< Installing app code >>>>>>>>>>\e[0m" | tee -a ${log}
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-  cd /app &>>${log}
-  unzip /tmp/${component}.zip &>>${log}
-  cd /app &>>${log}
-  pip3.6 install -r requirements.txt &>>${log}
-  echo -e "\e[36m <<<<<<<<<< restarting the services >>>>>>>>>>\e[0m" | tee -a ${log}
+  func_apppreq
+
+  echo -e "\e[36m>>>>>>>>>>>>  Build ${component} Service   <<<<<<<<<<<<\e[0m"
+  mvn clean package &>>${log}
+  mv target/${component}-1.0.jar ${component}.jar &>>${log}
+  func_exit_status
+
+  func_schema_setup
+
   func_systemd
-  
+}
+
+func_python() {
+  echo -e "\e[36m>>>>>>>>>>>>  Build ${component} Service   <<<<<<<<<<<<\e[0m"
+  yum install python36 gcc python3-devel -y &>>${log}
+  func_exit_status
+
+  func_apppreq
+
+  sed -i "s/rabbitmq_app_password/${rabbitmq_app_password}/" /etc/systemd/system/${component}.service
+
+  echo -e "\e[36m>>>>>>>>>>>>  Build ${component} Service   <<<<<<<<<<<<\e[0m"
+  pip3.6 install -r requirements.txt &>>${log}
+  func_exit_status
+
+  func_systemd
 }
